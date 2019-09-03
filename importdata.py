5e35ce5d8ce2cf1dc2a780e9d93740235e6f39e5
Mike <mcfneuro@gmail.com>
2019-09-03 11:17:05 -0400

    Updated database with ephys data

--- importdata.py
+++ importdata.py
@@ -4,40 +4,47 @@ import scipy.io as sio
 import numpy as np
 import pandas as pd
 
-behavfile = sio.loadmat('./datafiles/Sessions_select_20190415_01_pA_15-20_binsof5.mat')
+behavfile = sio.loadmat('./datafiles/Training_ephys_data.mat')
    
-behavdata = behavfile['Sessions_select']
+database = behavfile['Sessions_select']
 
-nth_rat = 1
 num_sess = 45; setns = 1
-# Session_data = np.ndarray([behavdata.size,1]);
+# Session_data = np.ndarray([database.size,1]);
 # Maybe is better to do a dictionary
 
 # Session_data_np = dict()
 
-# for nth_rat in range(0,behavdata.size):
+# for nth_rat in range(0,database.size):
 #     # This is the matrix of behav data for my nth subject:
-#     tmpdata = np.array(behavdata[0, nth_rat]['Sess_conc2'])
-#     tmpdata = tmpdata[0:num_sess,:]
-#     tmpname = np.array(behavdata[0, nth_rat]['Rat']); tmpname = tmpname[0]
-#     Session_data_np[tmpname]=tmpdata
+#     tmpBehav = np.array(database[0, nth_rat]['Sess_conc2'])
+#     tmpBehav = tmpBehav[0:num_sess,:]
+#     tmpname = np.array(database[0, nth_rat]['Rat']); tmpname = tmpname[0]
+#     Session_data_np[tmpname]=tmpBehav
 #     print('Fetched behav data from', tmpname)
 
 # New version of Session_data, using pandas
 Session_data = dict()
-for nth_rat in range(0,behavdata.size):     
+# Ephys_data = pd.DataFrame(columns=['x-corr', 'x-corr-CNO', 'BW20', 'BW20-CNO'])
+Ephys_data = np.full([database.size, 4], np.nan)    # pre-allocate for e-phys data
+
+for nth_rat in range(0,database.size):     
     # Subject ID:
-    tmpname = np.array(behavdata[0, nth_rat]['Rat']); tmpname = tmpname[0]
+    tmpname = np.array(database[0, nth_rat]['Rat']); tmpname = tmpname[0]
 
     # This is the matrix of behav data for my nth subject:
-    tmpdata = np.array(behavdata[0, nth_rat]['Sess_conc2'])     # this is a numpy array
+    tmpBehav = np.array(database[0, nth_rat]['Sess_conc2'])     # this is a numpy array
     if setns == 1:
-        tmpdata = tmpdata[0:num_sess,:] # if I want to standardize num of sessions per subject
+        tmpBehav = tmpBehav[0:num_sess,:] # if I want to standardize num of sessions per subject
 
-    tmpdata = pd.DataFrame(tmpdata, index=['session%s'%x for x in range(1,len(tmpdata)+1)],columns=['var%s'%x for x in range(1,tmpdata.shape[1]+1)])  
-    Session_data[tmpname] = tmpdata
-    print('Fetched behav data from', tmpname, ',', len(onepd), 'sessions')
+    tmpBehav = pd.DataFrame(tmpBehav, index=['session%s'%x for x in range(1,len(tmpBehav)+1)],columns=['var%s'%x for x in range(1,tmpBehav.shape[1]+1)])  
+    Session_data[tmpname] = tmpBehav
+    print('Fetched behav data from', tmpname, ',', len(tmpBehav), 'sessions')
+
+    # This is the matrix of ephys data for my nth subject:
+    tmpEphys = np.array(database[0, nth_rat]['ephys'])     # this is a numpy array
+    Ephys_data[nth_rat,:] = tmpEphys
 
 
+# print('Check var status')
 
 
